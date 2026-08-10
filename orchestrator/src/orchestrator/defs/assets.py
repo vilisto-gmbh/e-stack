2ac76e6b751d6895f21b406d23dcc53e4f1ec54d
Till Meinel <meinel@vilisto.de>
2026-08-10 11:24:31 +0200

    chore: adding data ingestion asset

--- orchestrator/src/orchestrator/defs/assets.py
+++ orchestrator/src/orchestrator/defs/assets.py
@@ -1,4 +1,5 @@
 import bambi as bmb
+import httpx
 import polars as pl
 import pandas as pd
 import dagster as dg
@@ -6,20 +7,25 @@ from pathlib import Path
 from typing import Any
 from .resources import PostgrestConnector
 
-INPUT_FILE = Path(__file__).parent/"data"/"example_1.csv"
+INPUT_FILE = Path(__file__).parent / "data" / "example_1.csv"
+
 
 @dg.asset
 def processed_csv(context: dg.AssetExecutionContext):
-    
-    df = pl.scan_csv(INPUT_FILE).with_columns(
-        (
-            pl.col("power") * pl.col("time").str.to_datetime().diff().dt.total_seconds()/3600
-        ).alias("energy"),
-        pl.lit("MWh").alias("energy_unit"),
-    ).collect()
-    context.log.info(
-        f"Processing of input file {INPUT_FILE} complete."
+
+    df = (
+        pl.scan_csv(INPUT_FILE)
+        .with_columns(
+            (
+                pl.col("power")
+                * pl.col("time").str.to_datetime().diff().dt.total_seconds()
+                / 3600
+            ).alias("energy"),
+            pl.lit("MWh").alias("energy_unit"),
+        )
+        .collect()
     )
+    context.log.info(f"Processing of input file {INPUT_FILE} complete.")
     context.add_output_metadata(
         {
             "num_rows": df.height,
@@ -27,16 +33,15 @@ def processed_csv(context: dg.AssetExecutionContext):
         }
     )
 
+
 @dg.asset
 async def raw_data(
-    context: dg.AssetExecutionContext,
-    postgrest: PostgrestConnector
-) -> Any: 
-    data_dict = await postgrest.get_json_async(
-        suburl="energy_data"        
-    )
+    context: dg.AssetExecutionContext, postgrest: PostgrestConnector
+) -> Any:
+    data_dict = await postgrest.get_json_async(suburl="energy_data")
     context.log.info(f"Extracted dict with {len(data_dict)} entries.")
     return data_dict
+
 
 @dg.asset(
     deps=["raw_data"],
@@ -45,16 +50,14 @@ def prediction_data(
     context: dg.AssetExecutionContext,
     raw_data: Any,
 ) -> pd.DataFrame:
-    
+
     data_df = pd.DataFrame(raw_data)
     context.log.info(f"Transformed raw data into a dataframe of length {len(data_df)}.")
 
     model = bmb.Model("power ~ th_amb + th_amb_prev_day + is_workday", data_df)
     context.log.info(f"Created the following Bayesian Model: {model}")
 
-    inf_data = model.fit(
-        chains=2
-    )
+    inf_data = model.fit(chains=2)
     context.log.info("Model fitting complete.")
 
     model.predict(inf_data, inplace=True, kind="response")
@@ -62,9 +65,9 @@ def prediction_data(
 
     post_pred = inf_data.posterior_predictive["power"]
     pred_df = data_df.assign(
-        power_pred = post_pred.mean(dim=("chain", "draw")).values,
-        power_pred_lo = post_pred.quantile(0.1, dim=("chain", "draw")).values,
-        power_pred_hi = post_pred.quantile(0.9, dim=("chain", "draw")).values,
+        power_pred=post_pred.mean(dim=("chain", "draw")).values,
+        power_pred_lo=post_pred.quantile(0.1, dim=("chain", "draw")).values,
+        power_pred_hi=post_pred.quantile(0.9, dim=("chain", "draw")).values,
     )
     context.log.info("Computation of power complete.")
     context.add_output_metadata(
@@ -73,5 +76,23 @@ def prediction_data(
             "processed_df": dg.MetadataValue.md(f"````\n{pred_df.head(3)}\n```"),
         }
     )
-    
+
     return pred_df
+
+
+@dg.asset(deps=["prediction_data"])
+async def ingest_prediction_data(
+    context: dg.AssetExecutionContext,
+    pred_data: pd.DataFrame,
+    postgrest: PostgrestConnector,
+) -> None:
+
+    pred_data_json = pred_data.to_json(orient="records")
+    pgrst_rsp = await postgrest.upsert_async(
+        suburl="energy_data",
+        json=pred_data_json,
+        on_conflict="time",
+    )
+    context.log.info(
+        f"Data ingest terminated with status code {pgrst_rsp.status_code}."
+    )
