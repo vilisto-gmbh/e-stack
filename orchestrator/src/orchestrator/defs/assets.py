338786d80bfd90adbbc4f60adf28226d02403728
Till Meinel <meinel@vilisto.de>
2026-08-10 12:54:54 +0200

    chore: completing etl pipeline job, dropping example asset

--- orchestrator/src/orchestrator/defs/assets.py
+++ orchestrator/src/orchestrator/defs/assets.py
@@ -1,36 +1,8 @@
 import bambi as bmb
-import polars as pl
 import pandas as pd
 import dagster as dg
-from pathlib import Path
 from typing import Any
 from .resources import PostgrestConnector
-
-INPUT_FILE = Path(__file__).parent / "data" / "example_1.csv"
-
-
-@dg.asset
-def processed_csv(context: dg.AssetExecutionContext):
-
-    df = (
-        pl.scan_csv(INPUT_FILE)
-        .with_columns(
-            (
-                pl.col("power")
-                * pl.col("time").str.to_datetime().diff().dt.total_seconds()
-                / 3600
-            ).alias("energy"),
-            pl.lit("MWh").alias("energy_unit"),
-        )
-        .collect()
-    )
-    context.log.info(f"Processing of input file {INPUT_FILE} complete.")
-    context.add_output_metadata(
-        {
-            "num_rows": df.height,
-            "processed_df": dg.MetadataValue.md(f"````\n{df.head(3)}\n```"),
-        }
-    )
 
 
 @dg.asset
@@ -86,7 +58,7 @@ async def ingest_prediction_data(
     postgrest: PostgrestConnector,
 ) -> None:
 
-    pred_data_json = prediction_data.to_json(orient="records")
+    pred_data_json = prediction_data.to_dict(orient="records")
     pgrst_rsp = await postgrest.upsert_async(
         suburl="energy_data",
         json=pred_data_json,
